53eca00d6976c572e9a26c2eae30f04f9526d203
mockingmoon <ipurujit@outlook.com>
2020-08-31 20:08:27 +0530

    custom dice size rolls

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,18 @@
 import random
 
 def main():
-  dice_rolls = 2
+  dice_size = int(input('How many sides are the dice? '))
+  dice_rolls = int(input('How many dices would you like to roll? '))
   dice_sum = 0
   for i in range(dice_rolls):
-    roll = random.randint(1,6)
+    roll = random.randint(1,dice_size)
     dice_sum += roll
-    print(f'You rolled a {roll}')
+    if roll == 1:
+      print(f'You rolled a {roll}:Critical Fail')
+    elif roll == dice_size:
+      print(f'You rolled a {roll}:Critical Success')
+    else:
+      print(f'You rolled a {roll}')
   print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
